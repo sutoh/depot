3df4f14e4b3bacfac1d7ac5d20a7e1c9ec53df20
Sutoh Shohei <sutoh.shohei@human-net.co.jp>
2012-04-04 22:02:12 +0900

    functionals

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '.sidebar-nav .nav-list a', minimum: 3
+    assert_select '.content .entry', 3
+    assert_select 'h3','Programming Ruby 1.9'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
